fa9a7395626669ae577efd991ecac2da481b6c3a
TomixJasina <148002314+TomixJasina@users.noreply.github.com>
2024-01-01 14:21:48 +0000

    Added array with questions

---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+//var for questions array
+let questionList = [
+    
+        {
+            title: 'What does HTML stand for?',
+            choices: ['Hyper Text Markup Language', 'High-level Text Machine Language', 'Hyperlink and Text Markup Language', 'Home Tool Markup Language'],
+            correctAnswer: 0
+        }
+    ,
+    
+         {
+            title: 'Which property is used to change the text color in CSS?',
+            choices: ['font-color', 'text-color', 'color', 'text-style'],
+            correctAnswer: 2
+        }
+    ,
+        {
+            title: 'What keyword is used to declare a variable in JavaScript?',
+            choices: ['variable', 'var', 'let', 'const'],
+            correctAnswer: 1
+        }
+    ,
+        {
+            title: 'What tag is used to create a hyperlink in HTML?',
+            choices: ['<link>', '<a>', '<href>', '<hyperlink>'],
+            correctAnswer: 0
+        }
+    ,
+        {
+            title: 'Which property is used to set the background color of an element in CSS?',
+            choices: ['bg-color', 'background-color', 'color-background', 'bgcolor'],
+            correctAnswer: 1
+        }
+    ,
+        {
+            title: 'What function is used to print content to the console in JavaScript?',
+            choices: ['display()', 'log()', 'print()', 'console.log()'],
+            correctAnswer: 3
+        }
+    ,
+        {
+            title: 'What does the <ul> tag stand for in HTML?',
+            choices: ['Unordered List', 'Universal Language', 'Underlined Link', 'Unique Line'],
+            correctAnswer: 0
+        }
+    ,
+        {
+            title: 'Which property is used to add spacing between letters in text?',
+            choices: ['letter-spacing', 'text-spacing', 'word-spacing', 'spacing'],
+            correctAnswer: 0
+        }
+]
